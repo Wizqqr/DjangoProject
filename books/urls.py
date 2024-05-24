7baa686d04338951d0a6ba57d61166bef7050dad
Wizqqr <baban1979molotov@gmail.com>
2024-05-24 20:54:27 +0600

    Пофиксил баги пятой домашки

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,23 @@
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('books/', views.BookList.as_view(), name = 'books'),
+#     path('books/<int:id>/', views.BookFullInfo.as_view(), name='fullaboutbooks'),
+#     path('books/<int:id>/update/', views.BookChange.as_view(), name='bookschange'),
+#     path('books/<int:id>/delete/', views.BookDelete.as_view(), name='booksdelete'),
+#     path('book_add/', views.BookAdd.as_view(), name='bookadd'),
+#     path('books/search/', views.SearchBookView.as_view(), name='book_search'),
+# ]
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.BookList.as_view(), name = 'books'),
+    path('books/', views.BookList.as_view(), name='books'),
     path('books/<int:id>/', views.BookFullInfo.as_view(), name='fullaboutbooks'),
+    path('books/search/', views.SearchBookView.as_view(), name='book_search'),
     path('books/<int:id>/update/', views.BookChange.as_view(), name='bookschange'),
     path('books/<int:id>/delete/', views.BookDelete.as_view(), name='booksdelete'),
     path('book_add/', views.BookAdd.as_view(), name='bookadd'),
-    path('search/', views.SearchBookView.as_view(), name='search'),
 ]
--- telephone/urls.py
+++ telephone/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('phone_list/<int:id>/update/', views.PhoneEditForm.as_view(), name='phone_update_view'),
     path('create_phone/', views.PhoneCreateForm.as_view(), name='create_phone'),
     path('phone_list/comment/', views.PhoneCreateComment.as_view(), name='create_comment'),
-    path('search/', views.SearchPhoneView.as_view(), name='search'),
+    path('search/', views.SearchPhoneView.as_view(), name='phone_search'),
 ]
 # urlpatterns = [
 #     path('phone_list/', views.PhoneListView.as_view(), name='phone_list'),
